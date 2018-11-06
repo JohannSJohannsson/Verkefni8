6ac8208f6ea142cf7ce784cff81bc43bd9cc6453
JohannSJohannsson <jsj13@hi.is>
2018-11-06 19:56:43 +0000

    Aftur að breyta um tölvu, kominn með allt nema add()

--- scripts.js
+++ scripts.js
@@ -22,7 +22,8 @@ const text = (() => {
       commit(e);
   });
   _items.addEventListener("keyup", commit);
-}
+  }
+
   function formHandler(e) {
     e.preventDefault();
     let strengur = document.querySelector(".form__input").value;
@@ -32,26 +33,38 @@ const text = (() => {
     document.querySelector(".form__input").value = "";
   }
 
-  // event handler fyrir það að klára færslu
+  // event handler fyrir það að checka við box
   function finish(e) {
     if(e.target.classList.contains("item__checkbox")){
       e.target.parentNode.classList.toggle("item--done");
     }
   }
 
-  // event handler fyrir það að breyta færslu
+  // event handler fyrir það að breyta texta við færslu
   function edit(e) {
-    document.getElementById("item__text").innerHTML = new HTML;
+    if(e.target.className === "item__text"){
+      const texti = e.target.textContent;
+      const textaskra = document.createElement("input");
+      textaskra.classList.add("item__edit");
+      textaskra.setAttribute("type", "text");
+      textaskra.value = texti;
+      e.target.parentNode.replaceChild(textaskra, e.target);
+    }
   }
 
-  // event handler fyrir það að klára að breyta færslu
+  // event handler fyrir það að klára að breyta texta við færslu
   function commit(e) {
-
-}
+    if((e.target.className === "item__edit")&&((e.key == "Enter"))) {
+      const breyttGildi = e.target.value;
+      const breyttSpan = document.createElement("span");
+      breyttSpan.classList.add("item__text");
+      breyttSpan.textContent = breyttGildi;
+      e.target.parentNode.replaceChild(breyttSpan, e.target);
+    }
+  }
 
   // fall sem sér um að bæta við nýju item
   function add(value) {
-s
   }
 
   // event handler til að eyða færslu
